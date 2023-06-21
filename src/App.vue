8db32501d87c16ab396ff1c836b7a7d71afee5f9
Andrew <andrew.suponev@gmail.com>
2023-06-21 15:01:51 +0400

    feat: implement simple home page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <AppHeader />
   <RouterView />
+  <AppFooter />
 </template>
+
+<style>
+#app {
+  @apply flex min-h-screen flex-col;
+}
+
+header,
+footer {
+  @apply shrink-0 grow-0;
+}
+
+main {
+  @apply grow;
+}
+
+.container {
+  @apply mx-auto max-w-[1440px] px-5;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <main>
-    <div class="text-xl font-bold text-red-500">Hello</div>
+  <main class="flex items-center justify-center">
+    <div class="container">
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="font-bold">Добро пожаловать на сайт Simple To Do App!</h1>
+        <p class="my-5">
+          На странице Задачи вы сможете создавать свои задачи и управлять ими
+        </p>
+        <RouterLink
+            to="/todo"
+            class="text-sky-600 hover:text-sky-800 mr-4"
+          >
+            Перейти на страницу Задачи
+          </RouterLink>
+      </div>
+    </div>
   </main>
 </template>
