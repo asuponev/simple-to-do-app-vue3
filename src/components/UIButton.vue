813231657207a907941541cb009940583d2b59c7
Andrew <andrew.suponev@gmail.com>
2023-06-21 23:52:50 +0400

    refactor: add component for primary button

---
+++ src/components/UIButton.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps<{
+  submitBtn?: boolean
+}>()
+</script>
+
+<template>
+  <button
+    :type="submitBtn ? 'submit' : 'button'"
+    class="bg-blue-500 w-40 px-4 py-3 rounded text-white hover:bg-blue-600 transition-colors duration-200 outline-none"
+  >
+    <slot />
+  </button>
+</template>
