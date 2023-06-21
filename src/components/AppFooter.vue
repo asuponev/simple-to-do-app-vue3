b7604757b7c8bbaffbb3785a0d07164242fe93bb
Andrew <andrew.suponev@gmail.com>
2023-06-21 14:50:50 +0400

    feat: add simple header and footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+    <footer>
+    <div class="container">
+      <div class="py-5 flex items-center justify-end">
+        <span class="mr-3">Developer</span>
+        <a
+          href="https://github.com/elsuppo/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-sky-600 hover:text-sky-800"
+        >
+          Andrew Suponev
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="py-5 flex items-center justify-between">
+        <div class="font-bold">Simple To Do App</div>
+        <nav class="flex items-center">
+          <RouterLink
+            to="/"
+            class="text-sky-600 hover:text-sky-800 mr-4"
+          >
+            Главная
+          </RouterLink>
+          <RouterLink
+            to="/todo"
+            class="text-sky-600 hover:text-sky-800"
+          >
+            Задачи
+          </RouterLink>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
