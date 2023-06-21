b4f4708b44016b2d4876241de9e4a78003a36f08
Andrew <andrew.suponev@gmail.com>
2023-06-21 23:55:34 +0400

    feat: update home styles

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import UIButton from '@/components/UIButton.vue';
 </script>
 
 <template>
-  <main class="flex items-center justify-center">
-    <div class="container">
+  <main class="flex items-center justify-center mt-16">
+    <div class="container max-w-2xl">
       <div class="flex flex-col items-center justify-center">
-        <h1 class="font-bold">Добро пожаловать на сайт Simple To Do App!</h1>
-        <p class="my-5">
-          На странице Задачи вы сможете создавать свои задачи и управлять ими
+        <h1 class="font-bold text-center text-xl">
+          Добро пожаловать на сайт Simple To Do App!
+        </h1>
+        <p class="my-5 text-center">
+          На странице Задачи вы сможете создавать свои задачи. Добавленные
+          задачи сохраняются в хранилище браузера, поэтому вы можете управлять
+          ими после обновления страницы.
         </p>
-        <RouterLink
-            to="/todo"
-            class="text-sky-600 hover:text-sky-800 mr-4"
-          >
-            Перейти на страницу Задачи
-          </RouterLink>
+        <UIButton @click="$router.push('/todo')">Добавить задачи</UIButton>
       </div>
     </div>
   </main>
